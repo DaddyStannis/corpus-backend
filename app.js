806333a75bdc3dd85d14a5ceb98e17f634e54127
Stanislav Sakun <stanislav.sakun@ukr.net>
2023-08-25 22:08:05 +0300

    Added cors options

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ import productRouter from "./routes/productRoutes.js";
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
 
